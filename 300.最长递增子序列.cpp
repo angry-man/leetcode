c05c6d2f0f1f43ca1cf9343faf16dcf91688753a
angry-man <1678326393@qq.com>
2021-11-26 13:36:38 +0800

    300和376

---
+++ 300.最长递增子序列.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=cpp
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        if(nums.size()<2)
+            return nums.size();
+        vector<int> dp(nums.size(),1);
+        int max_len = 0;
+        for(int i = 1;i < nums.size();i++)
+        {
+            int max_j = 0;
+            for(int j = 0;j < i;j++)
+            {
+                if(nums[j] < nums[i])
+                    max_j = max(max_len,dp[j]);
+            }
+            dp[i] = max_j + 1;
+            max_len = max(max_len,dp[i]);
+        }
+        return max_len;
+    }
+};
+// @lc code=end
+
--- 376.摆动序列.cpp
+++ 376.摆动序列.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem376.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=376 lang=cpp
  *
@@ -9,8 +17,33 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) 
     {
-        
+        int n = nums.size();
+        if(n<2)
+            return n;
+        int pre_up = 1;
+        int pre_down = 1;
+        int cur_up, cur_down;
+        for(int i = 1;i < n;i++)
+        {  
+            if(nums[i]>nums[i-1])
+            {
+                cur_up = max(pre_up,pre_down+1);
+                cur_down = pre_down;
+            }
+            else if(nums[i] == nums[i-1])
+            {
+                cur_up = pre_up;
+                cur_down = pre_down;
+            }
+            else
+            {
+                cur_up = pre_up;
+                cur_down = max(pre_up+1,pre_down);
+            }
+            pre_down = cur_down;
+            pre_up = cur_up;
+        }
+        return max(cur_down,cur_up);
     }
 };
 // @lc code=end
-
