8ee9d82fd984d62b959ba0def791b358d40c4a18
殷健 - yinjian <1678326393@qq.com>
2022-01-21 15:20:47 +0800

    数据结构入门第12天

---
+++ 数据结构入门/112.路径总和.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        if(root == nullptr)
+            return false;
+        if(root->left == nullptr && root->right == nullptr)
+        {
+            if(root->val == targetSum)
+                return true;
+            else 
+                return false;
+        }
+        bool left = false;
+        bool right = false;
+        if(root->left != nullptr)
+        {
+            left = hasPathSum(root->left,targetSum - root->val);
+            if(left)
+                return true;
+        }  
+        if(root->right != nullptr)
+            right = hasPathSum(root->right,targetSum - root->val);
+        return left || right;             
+    }
+};
+// @lc code=end
+
